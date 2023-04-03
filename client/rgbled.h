ed55fbbbd0be22e9b424daf627ad0275db427d72
raphiniert <dev@raphiniert.eu>
2023-04-03 14:19:25 +0200

    define pins and some basic colors

---
+++ client/rgbled.h
@@ -0,0 +1,9 @@
+// Arduino Nano 33 IoT
+#define RED_PIN 5
+#define GREEN_PIN 3
+#define BLUE_PIN 2
+
+// Colors
+#define STATUS_OK "#00ff00"      // green
+#define STATUS_ERROR "#ff0000"   // red
+#define STATUS_PENDING "#ff0a00" // somewhat orange
